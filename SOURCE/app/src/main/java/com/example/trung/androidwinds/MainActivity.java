f40533f9a1b1ccc18fd2e06ae9f15b5cdecb01db
TrungDaniel <trungdaniel1998@gmail.com>
2019-05-13 21:27:38 +0700

    bla bla bla

--- SOURCE/app/src/main/java/com/example/trung/androidwinds/MainActivity.java
+++ SOURCE/app/src/main/java/com/example/trung/androidwinds/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.trung.androidwinds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        // tao doi tuong user
+        User windsUser = new User();
+        windsUser.setId(1);
+        windsUser.setPhoneNumber("0387786767");
+        windsUser.setProfileUrl("http://pornhub.com/avatar.ipg");
+        windsUser.setUsername("Trung");
+
+        //Toast.makeText(this, ""+windsUser.getUsername(), Toast.LENGTH_LONG).show();
+        // sử dụng đối tượng được khai báo và khởi tạo trong file xml
+        /*
+            bước 1 : gán id cho đối tượng trong xml
+            bước 2 : tạo tên mới và gán lại cho đối tượng ;
+
+         */
+        EditText edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        edtPhoneNumber.setText("0387786767");
+
     }
 }
---
+++ SOURCE/app/src/main/java/com/example/trung/androidwinds/User.java
@@ -0,0 +1,39 @@
+package com.example.trung.androidwinds;
+
+public class User {
+    int id;
+    String username,phoneNumber,profileUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+}
+
