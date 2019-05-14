d12152d5d87b29a67135e7c2b24fa2f295577124
TrungDaniel <trungdaniel1998@gmail.com>
2019-05-14 11:47:20 +0700

    update UI

--- SOURCE/app/src/main/java/com/example/trung/androidwinds/MainActivity.java
+++ SOURCE/app/src/main/java/com/example/trung/androidwinds/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.trung.androidwinds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         windsUser.setProfileUrl("http://pornhub.com/avatar.ipg");
         windsUser.setUsername("Trung");
 
+        final EditText edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        TextView tvDangNhap = findViewById(R.id.tv_dang_nhap);
         //Toast.makeText(this, ""+windsUser.getUsername(), Toast.LENGTH_LONG).show();
         // sử dụng đối tượng được khai báo và khởi tạo trong file xml
         /*
@@ -26,8 +31,23 @@ public class MainActivity extends AppCompatActivity {
             bước 2 : tạo tên mới và gán lại cho đối tượng ;
 
          */
-        EditText edtPhoneNumber = findViewById(R.id.edt_phone_number);
-        edtPhoneNumber.setText("0387786767");
+        // function click textview dang nhap
+        tvDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtPhoneNumber.length()==0){
+                    edtPhoneNumber.setHint("!");
+                    edtPhoneNumber.setHintTextColor(getResources().getColor(R.color.colorRed));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "bla bla", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
 
     }
+
+
 }
