a479ef2723e7c7749a74f41c227bdf21f91c0f2f
TrungDaniel <trungdaniel1998@gmail.com>
2019-05-28 20:57:04 +0700

    update home activity

--- SOURCE/app/src/main/java/com/example/trung/androidwinds/HomeActivity.java
+++ SOURCE/app/src/main/java/com/example/trung/androidwinds/HomeActivity.java
@@ -18,8 +18,8 @@ import java.io.InputStream;
 
 public class HomeActivity extends AppCompatActivity {
     TextView tvUsername, tvPhoneNumber, tvDangXuat;
-    ImageView imgNews1, imgNews2, imgNews3;
-    TextView tvNews1, tvNews2, tvNews3;
+    ImageView imgNews1, imgNews2, imgNews3, imgCtth1, imgCtth2, imgCtth3;
+    TextView tvNews1, tvNews2, tvNews3, tvCtth1, tvCtth2, tvCtth3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +43,12 @@ public class HomeActivity extends AppCompatActivity {
             JSONObject jsonCustomerDetail = jsonResult.getJSONObject("customerDetail");
             String customerName = jsonCustomerDetail.getString("customerName");
             JSONArray jsonArrayNews = jsonResult.getJSONArray("listNews");
+            JSONArray jsonArrayCtth = jsonResult.getJSONArray("listPromotion");
             // -------------------
             JSONObject jsonNew1 = jsonArrayNews.getJSONObject(0);
             JSONObject jsonNew2 = jsonArrayNews.getJSONObject(1);
             JSONObject jsonNew3 = jsonArrayNews.getJSONObject(2);
+
             //
             String newsTitle1 = jsonNew1.getString("title");
             String urlNew1 = jsonNew1.getString("urlImage");
@@ -56,18 +58,40 @@ public class HomeActivity extends AppCompatActivity {
 
             String newsTitle3 = jsonNew3.getString("title");
             String urlNew3 = jsonNew3.getString("urlImage");
+            //
 
+            JSONObject jsonCtth1 = jsonArrayCtth.getJSONObject(0);
+            JSONObject jsonCtth2 = jsonArrayCtth.getJSONObject(1);
+            JSONObject jsonCtth3 = jsonArrayCtth.getJSONObject(2);
+
+            String ctthTitle1 = jsonCtth1.getString("title");
+            String urlCtth1 = jsonCtth1.getString("urlImage");
+
+            String ctthTitle2 = jsonCtth2.getString("title");
+            String urlCtth2 = jsonCtth2.getString("urlImage");
+
+            String ctthTitle3 = jsonCtth3.getString("title");
+            String urlCtth3 = jsonCtth3.getString("urlImage");
 
             //
             Picasso.get().load(urlNew1).placeholder(R.mipmap.img_default).into(imgNews1);
             Picasso.get().load(urlNew2).placeholder(R.mipmap.img_default).into(imgNews2);
             Picasso.get().load(urlNew3).placeholder(R.mipmap.img_default).into(imgNews3);
 
+            Picasso.get().load(urlCtth1).placeholder(R.mipmap.img_default).into(imgCtth1);
+            Picasso.get().load(urlCtth2).placeholder(R.mipmap.img_default).into(imgCtth2);
+            Picasso.get().load(urlCtth3).placeholder(R.mipmap.img_default).into(imgCtth3);
+
 
 
             tvNews1.setText(newsTitle1);
             tvNews2.setText(newsTitle2);
             tvNews3.setText(newsTitle3);
+
+            tvCtth1.setText(ctthTitle1);
+            tvCtth2.setText(ctthTitle2);
+            tvCtth3.setText(ctthTitle3);
+
 
 
         } catch (JSONException e) {
@@ -132,5 +156,11 @@ public class HomeActivity extends AppCompatActivity {
         tvNews1 = findViewById(R.id.tv_new_1);
         tvNews2 = findViewById(R.id.tv_new_2);
         tvNews3 = findViewById(R.id.tv_new_3);
+        imgCtth1 = findViewById(R.id.img_ctth1);
+        imgCtth2 = findViewById(R.id.img_ctth2);
+        imgCtth3 = findViewById(R.id.img_ctth3);
+        tvCtth1 = findViewById(R.id.tv_ctth1);
+        tvCtth2 = findViewById(R.id.tv_ctth2);
+        tvCtth3 = findViewById(R.id.tv_ctth3);
     }
 }
